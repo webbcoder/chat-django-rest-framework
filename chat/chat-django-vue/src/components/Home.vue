09902cc092be6893d77d8590229db75be8c4382a
webbcoder <ksk.coder@gmail.com>
2019-11-12 23:30:37 +0200

    - create component Home - create component Login

---
+++ chat/chat-django-vue/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Chat room</h1>
+        <button @click="goLogin">Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        methods: {
+            goLogin(){
+                this.$router.push({name: 'login'})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ chat/chat-django-vue/src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input v-model="login" placeholder="Login"/>
+        <input v-model="password" placeholder="Password"/>
+        <button>Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data(){
+            return{
+                login: '',
+                password: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
