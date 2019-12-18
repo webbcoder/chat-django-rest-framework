b586a0447b98aca2c6e30f70339c4273da139eeb
webbcoder <ksk.coder@gmail.com>
2019-12-18 11:23:07 +0200

    - change method sendMessage

--- chat/chat-django-vue/src/components/rooms/Dialog.vue
+++ chat/chat-django-vue/src/components/rooms/Dialog.vue
@@ -90,7 +90,7 @@
                     console.log(response);
                     this.loadDialog()
                 })
-                .cathe(error => {
+                .catch(error => {
                     alert(error.statusText)
                 })
             }
